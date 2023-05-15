2692a382714a270b0e9509b0e44f5cca1b102080
Roman Strashkin <roman.strashkin@gmail.com>
2023-05-15 11:55:52 +0300

    lualine: customization; show hex-code of character under cursor

--- lua/core/plugins_config/lualine.lua
+++ lua/core/plugins_config/lualine.lua
@@ -1,5 +1,20 @@
+local function location()
+    local cursor = vim.fn.getcurpos()
+    local line = vim.fn.getline('.')
+    local pos = cursor[3]
+    local line_num = cursor[2]
+    if line == "" then
+        return string.format('line=%d|pos=%d|hex=0x00', line_num, pos)
+    end
+
+    local character = string.sub(line, pos, pos)
+    local ascii_value = string.byte(character)
+    return string.format('line=%d|pos=%d|hex=0x%x', line_num, pos, ascii_value)
+end
+
 require('lualine').setup({
   options = {
     theme = 'material',
   },
+  sections = { lualine_z = {location} },
 })
