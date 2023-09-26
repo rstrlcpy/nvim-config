0747caa2c40eed765c1af380c8e2e8a96c5df044
Roman Strashkin <roman.strashkin@gmail.com>
2023-09-26 09:55:45 +0300

    lualine: fixed hex-code display

--- lua/core/plugins_config/lualine.lua
+++ lua/core/plugins_config/lualine.lua
@@ -3,13 +3,13 @@ local function location()
     local line = vim.fn.getline('.')
     local pos = cursor[3]
     local line_num = cursor[2]
-    if line == "" then
+    local character = string.sub(line, pos, pos)
+    if character == "" then
         return string.format('line=%d|pos=%d|hex=0x00', line_num, pos)
     end
 
-    local character = string.sub(line, pos, pos)
     local ascii_value = string.byte(character)
-    return string.format('line=%d|pos=%d|hex=0x%x', line_num, pos, ascii_value)
+    return string.format('line=%d|pos=%d|hex=0x%-2x', line_num, pos, ascii_value)
 end
 
 require('lualine').setup({
